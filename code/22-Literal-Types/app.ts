3fc5d5b7aec41d0c82bda308a9245b4315e34c95
Chuk Ovuworie <chuk.ovuworie@dotevoke.com>
2020-02-18 13:17:17 -0600

    22. Literal Types

---
+++ code/22-Literal-Types/app.ts
@@ -0,0 +1,26 @@
+function combine(
+  input1: number | string,
+  input2: number | string,
+  // these literal types are the only allowed values
+  resultConversion: "as-number" | "as-text"
+) {
+  let result: number | string;
+  if (
+    (typeof input1 === "number" && typeof input2 === "number") ||
+    resultConversion === "as-number"
+  ) {
+    result = +input1 + +input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combinedAges = combine(30, 26, "as-number");
+console.log(combinedAges);
+
+const combinedStringAges = combine("30", "26", "as-number");
+console.log(combinedStringAges);
+
+const combinedNames = combine("Max", "Anna", "as-text");
+console.log(combinedNames);
