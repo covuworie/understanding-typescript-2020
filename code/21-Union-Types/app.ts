beb48c241ed58255d50abb25ae754a45017882b9
Chuk Ovuworie <chuk.ovuworie@dotevoke.com>
2020-02-18 12:56:25 -0600

    21. Union Types

---
+++ code/21-Union-Types/app.ts
@@ -0,0 +1,15 @@
+function combine(input1: number | string, input2: number | string) {
+    let result: number | string;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Anna');
+console.log(combinedNames);
