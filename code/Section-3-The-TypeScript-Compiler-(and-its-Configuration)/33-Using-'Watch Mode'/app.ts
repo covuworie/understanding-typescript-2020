71994f6bbdc1736f33598caf3de14fa1b16bafd4
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-20 18:46:28 -0600

    33. Using "Watch Mode"

---
+++ code/Section-3-The-TypeScript-Compiler-(and-its-Configuration)/33-Using-'Watch Mode'/app.ts
@@ -0,0 +1,6 @@
+// run the following in the terminal:
+// node_modules\.bin\tsc "code\Section-3-The-TypeScript-Compiler-(and-its-Configuration)\33-Using-'Watch Mode'\app.ts" --watch
+
+const userName = 'Maximilian';
+
+console.log(userName);
