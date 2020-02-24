b17c64de302f12e5a4ec7c2cac27a0dc7842a1f7
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-24 13:34:23 -0600

    48. "let" and "const"

---
+++ code/Section-4-Next-generation-JavaScript-&-TypeScript/48-'let'-and-'const'/src/app.ts
@@ -0,0 +1,20 @@
+const userName = 'Max';
+
+let age = 30;
+
+age = 29;
+var result;
+
+function add(a: number, b: number) {
+    let result;
+    result = a + b;
+    return result;
+}
+
+// console.log(result);
+
+// if(age > 20) {
+//     let isOld = true;
+// }
+
+// console.log(isOld);
