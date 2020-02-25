bea6ccdf790660415e9b7ce5752a21b5f5df9831
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-25 12:22:05 -0600

    52. Rest Parameters

---
+++ code/Section-4-Next-generation-JavaScript-&-TypeScript/52-Rest-Parameters/src/app.ts
@@ -0,0 +1,38 @@
+const userName = 'Max';
+
+let age = 30;
+
+age = 29;
+var result;
+
+// const add = (a: number, b: number = 1) => a + b;  // implicit return statement
+
+// const printOutput: (a: number | string) => void = output => console.log(output);
+
+// const button = document.querySelector('button');
+
+// if (button) {
+//     button.addEventListener('click', event => console.log(event));
+// }
+
+// printOutput(add(5));
+
+const hobbies = ['Sports', 'Cooking'];
+const activeHobbies = ['Hiking'];
+activeHobbies.push(...hobbies);
+
+const person = {
+    name: 'Max',
+    age: 30
+}
+
+const copiedPerson = { ...person };
+
+const add = (...numbers: number[]) => {
+    return numbers.reduce((curResult, curValue) => {
+        return curResult + curValue;
+    }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
