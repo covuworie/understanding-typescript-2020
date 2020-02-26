c019f8934b72afce5ffeb0dd75c6a48ae13e37f1
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-02-26 12:17:50 -0600

    63. "readonly" Properties

---
+++ code/Section-5-Classes-&-Interfaces/63-'readonly'-Properties/src/app.ts
@@ -0,0 +1,40 @@
+class Department {
+    // private readonly id: string;
+    // private name: string;
+    private employees: string[] = [];
+
+    constructor(private readonly id: string, public name: string) {
+        // this.id = id;
+        // this.name = name;
+    }
+
+    describe(this: Department) {
+        console.log(`Department: (${this.id}): ${this.name}`);
+    }
+
+    addEmployee(employee: string) {
+        // validation etc
+        // this.id = 'd2';  // cannot change readonly property
+        this.employees.push(employee);
+    }
+
+    printEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee('Max');
+accounting.addEmployee('Manu');
+
+// accounting.employees[2] = 'Anna';  // cannot access private member
+
+accounting.describe();
+accounting.name = 'NEW NAME';
+accounting.printEmployeeInformation();
+
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+
+// accountingCopy.describe();
