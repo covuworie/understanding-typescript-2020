d49345a203929c3f67fb54fdcd1928b32956ebd3
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-03-04 13:27:36 -0600

    105. Working with Decorator Factories

---
+++ code/Section-8-Decorators/105-Working-With-Decorator-Factories/src/app.ts
@@ -0,0 +1,20 @@
+// Decorator Factory allows us to now pass in parameters that will be used by the decorator function (the inner function). Note that the factory returns the decorator function and now we must invoke the factory below.
+function Logger(logString: string) {
+    return function(constructor: Function) {
+        console.log(logString);
+        console.log(constructor);
+    }
+}
+
+@Logger('LOGGING - PERSON')
+class Person {
+    public name = 'Max';
+
+    public constructor() {
+        console.log('Creating person object...');
+    }
+}
+
+const person = new Person();
+
+console.log(person);
