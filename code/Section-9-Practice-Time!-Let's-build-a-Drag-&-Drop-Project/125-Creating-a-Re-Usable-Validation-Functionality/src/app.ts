2df0e67baae278021f1aba18b0aace19c9ccf2f1
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-03-11 13:32:37 -0500

    125. Creating a Re-Usable Validation Functionality

---
+++ code/Section-9-Practice-Time!-Let's-build-a-Drag-&-Drop-Project/125-Creating-a-Re-Usable-Validation-Functionality/src/app.ts
@@ -0,0 +1,141 @@
+// Validation
+interface Validatable {
+  value: string | number;
+  required?: boolean;
+  minLength?: number;
+  maxLength?: number;
+  min?: number;
+  max?: number;
+}
+
+function validate(input: Validatable) {
+  let isValid = true;
+  if (input.required) {
+    isValid = isValid && input.value.toString().trim().length !== 0;
+  }
+  if (input.minLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.length >= input.minLength;
+  }
+  if (input.maxLength != null && typeof input.value === "string") {
+    isValid = isValid && input.value.length <= input.maxLength;
+  }
+  if (input.min != null && typeof input.value === "number") {
+    isValid = isValid && input.value >= input.min;
+  }
+  if (input.max != null && typeof input.value === "number") {
+    isValid = isValid && input.value <= input.max;
+  }
+  return isValid;
+}
+
+// autobind decorator
+function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    }
+  };
+  return adjustedDescriptor;
+}
+
+// ProjectInput Class
+class ProjectInput {
+  private templateElement: HTMLTemplateElement;
+  private hostElement: HTMLDivElement;
+  private formElement: HTMLFormElement;
+
+  private titleInputElement: HTMLInputElement;
+  private descriptionInputElement: HTMLInputElement;
+  private peopleInputElement: HTMLInputElement;
+
+  public constructor() {
+    this.templateElement = document.querySelector(
+      "#project-input"
+    ) as HTMLTemplateElement;
+    this.hostElement = document.querySelector("#app") as HTMLDivElement;
+
+    const importNode = document.importNode(this.templateElement.content, true);
+    this.formElement = importNode.firstElementChild as HTMLFormElement;
+    this.formElement.id = "user-input";
+
+    this.titleInputElement = this.formElement.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.formElement.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.formElement.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    this.attachFormListener();
+    this.renderForm();
+  }
+
+  private getInputs(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeople = this.peopleInputElement.value;
+
+    const titleValidator: Validatable = {
+      value: enteredTitle,
+      required: true
+    };
+    const descriptionValidator: Validatable = {
+      value: enteredDescription,
+      required: true,
+      minLength: 5
+    };
+    const peopleValidator: Validatable = {
+      value: +enteredPeople,
+      required: true,
+      min: 1,
+      max: 5
+    };
+
+    if (
+      !validate(titleValidator) ||
+      !validate(descriptionValidator) ||
+      !validate(peopleValidator)
+    ) {
+      alert("Invalid input, please try again");
+      return;
+    } else {
+      return [enteredTitle, enteredDescription, +enteredPeople];
+    }
+  }
+
+  private clearInputs() {
+    this.titleInputElement.value = "";
+    this.descriptionInputElement.value = "";
+    this.peopleInputElement.value = "";
+  }
+
+  @autobind
+  private submitListener(event: Event) {
+    event.preventDefault();
+    const userInput = this.getInputs();
+    if (Array.isArray(userInput)) {
+      const [title, description, people] = userInput;
+      console.log(title, description, people);
+      this.clearInputs();
+    }
+  }
+
+  private attachFormListener() {
+    this.formElement.addEventListener("submit", this.submitListener);
+  }
+
+  private renderForm() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.formElement);
+  }
+}
+
+const projectInput = new ProjectInput();
