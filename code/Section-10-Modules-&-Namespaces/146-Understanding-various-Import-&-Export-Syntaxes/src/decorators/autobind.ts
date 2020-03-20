d151222695dd3af95b0992f3d012d323170658de
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-03-20 13:12:27 -0500

    146. Understanding various Import & Export Syntaxes

---
+++ code/Section-10-Modules-&-Namespaces/146-Understanding-various-Import-&-Export-Syntaxes/src/decorators/autobind.ts
@@ -0,0 +1,16 @@
+// autobind decorator
+export function autobind(
+  _target: any,
+  _methodName: string,
+  descriptor: PropertyDescriptor
+) {
+  const originalMethod = descriptor.value;
+  const adjustedDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunction = originalMethod.bind(this);
+      return boundFunction;
+    }
+  };
+  return adjustedDescriptor;
+}
