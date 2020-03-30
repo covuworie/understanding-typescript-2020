e9791305285fa7dcbeda55cd50d2f8356644fd75
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-03-30 14:09:27 -0500

    155. Finishing the Setup and Adding webpack-dev-server

---
+++ code/Section-11-Using-Webpack-with-TypeScript/155-Finishing-the-Setup-and-Adding-webpack-dev-server/src/app.ts
@@ -0,0 +1,7 @@
+import { ProjectInput } from "./components/project-input";
+import { ProjectList } from "./components/project-list";
+
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
+
