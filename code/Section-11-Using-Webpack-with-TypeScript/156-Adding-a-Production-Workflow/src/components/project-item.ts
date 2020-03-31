c5b88cb6a9070c6075a8a5677571fe9f3c44895c
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-03-31 11:43:10 -0500

    156. Adding a Production Workflow

---
+++ code/Section-11-Using-Webpack-with-TypeScript/156-Adding-a-Production-Workflow/src/components/project-item.ts
@@ -0,0 +1,47 @@
+import { Draggable } from "../models/drag-drop";
+import { Project } from "../models/project";
+import Component from "./base-component";
+import { autobind } from "../decorators/autobind";
+
+// ProjectItem Class
+export class ProjectItem extends Component<HTMLUListElement, HTMLLIElement>
+  implements Draggable {
+  private project: Project;
+
+  public get persons() {
+    if (this.project.people == 1) {
+      return "1 person";
+    }
+    return `${this.project.people} people`;
+  }
+
+  public constructor(hostElementId: string, project: Project) {
+    super("single-project", hostElementId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderProjectListContent();
+  }
+
+  @autobind
+  public dragStartListener(event: DragEvent) {
+    // We only transfer the id to save memory. This is sufficient to retrieve the object data when we drop it.
+    event.dataTransfer!.setData("text/plain", this.project.id);
+    event.dataTransfer!.effectAllowed = "move";
+  }
+
+  public dragEndListener(_: DragEvent) {
+    console.log("DragEnd");
+  }
+
+  public configure() {
+    this.element.addEventListener("dragstart", this.dragStartListener);
+    this.element.addEventListener("dragend", this.dragEndListener);
+  }
+
+  public renderProjectListContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
