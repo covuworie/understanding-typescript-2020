7b3a5f3f5fadedff96a91eda9f9816742d8cf4b8
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-04-08 13:06:34 -0500

    184. Types for Other TypeScript Features (e.g. Redux or Routing)

---
+++ code/Section-14-React-js-and-TypeScript/184-types-for-other-react-features-eg-redux-or-routing/src/App.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+// import { Route } from "react-router-dom";
+
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+}
+
+export default App;
