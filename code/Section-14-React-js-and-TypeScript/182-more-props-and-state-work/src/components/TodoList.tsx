fb490de05b3c6b681915c32f2b8003d46479c4e5
Chuk Ovuworie <chuk.ovuworie@gmail.com>
2020-04-08 12:36:34 -0500

    182. More Props & State Work

---
+++ code/Section-14-React-js-and-TypeScript/182-more-props-and-state-work/src/components/TodoList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+interface TodoListProps {
+  items: { id: string; text: string }[];
+  onDeleteTodo: (id: string) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = (props) => {
+  return (
+    <ul>
+      {props.items.map((todo) => (
+        <li key={todo.id}>
+          <span>{todo.text}</span>
+          <button onClick={props.onDeleteTodo.bind(null, todo.id)}>
+            DELETE
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
